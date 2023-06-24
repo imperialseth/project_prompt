3373475f602796a10a55105f872edef4277e91fe
mtopp <madstopp@gmail.com>
2023-06-24 21:33:04 +0200

    first commit

---
+++ components/Provider.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react'
+
+import { SessionProvider } from 'next-auth/react';
+
+const Provider = ({ children, session }) => {
+  return (
+    <SessionProvider sesion={session}>
+      {children}
+    </SessionProvider>
+  )
+}
+
+export default Provider
